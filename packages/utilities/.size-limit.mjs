0ac4d933e1eb2088eb481d47d14592ae0ccec342
Olavo Amorim Santos <olavo.a.santos@gmail.com>
2025-02-15 16:21:20 -0600

    remove size limit configuration on deleted submodule

--- packages/utilities/.size-limit.mjs
+++ packages/utilities/.size-limit.mjs
@@ -3,5 +3,4 @@ import {sizeLimit} from '../../.size-limit.mjs';
 export default sizeLimit(
   // Add submodules
   {path: 'dist/isRecord.js', limit: '55 b'},
-  {path: 'dist/index.js', limit: '100 b'},
 );
