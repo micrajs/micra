744b8b3ae5bb1948bc272bd7bfa2d72b5d737e7f
Olavo Amorim Santos <olavo.a.santos@gmail.com>
2025-04-19 19:43:10 -0500

    performance tests

--- packages/environment/.size-limit.mjs
+++ packages/environment/.size-limit.mjs
@@ -2,5 +2,5 @@ import {sizeLimit} from '../../.size-limit.mjs';
 
 export default sizeLimit(
   // Add submodules
-  {path: 'dist/index.js', limit: '13 b'},
+  {path: 'dist/index.js', limit: '1232 b'},
 );
